f79d649306cb694be40f7e4cbb68cdf367f06fbc
niharika-vdeshmukh <niharikad2209@gmail.com>
2023-01-22 03:54:42 -0500

    product list display

---
+++ angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,15 @@
+export class Product {
+
+    constructor(public sku: string,
+                public name: string,
+                public description: string,
+                public unit_price: number,
+                public image_url: string,
+                public active: boolean,
+                public units_in_stock: number,
+                public date_created: Date,
+                public last_updated: Date
+        ) {
+
+    }
+}
