645acfbe2f409dd698337eede2dbe398a2670832
niharika-vdeshmukh <niharikad2209@gmail.com>
2023-01-22 19:29:06 -0500

    search products by categoryId

--- ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
+++ ecommerce/src/main/java/com/example/ecommerce/dao/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.example.ecommerce.dao;
 
 import com.example.ecommerce.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -9,4 +12,5 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "product", path = "product")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
